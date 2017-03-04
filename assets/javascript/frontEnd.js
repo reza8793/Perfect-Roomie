a6ab8903a1381fdfffefe68f596793acaa49c608
reza8793 <aabed525@gmail.com>
2017-03-04 15:53:24 -0800

    front end working

---
+++ assets/javascript/frontEnd.js
@@ -0,0 +1,40 @@
+ 
+
+
+ $(document).ready(function(){
+
+ 	var zipValue;
+
+
+ 	$('#findroomiebutton').on('click', function(){
+
+                    console.log("find your roomie button clicked");
+                // This line of code will grab the input from the textbox...
+
+            });
+
+
+ 	$('#zipbutton').on('click', function(){
+
+                    console.log("find matches button clicked");
+
+                   // location.href = "survey.html";
+
+
+                     zipValue = $('#zipinput').val().trim();
+
+
+                     console.log( "zip code is " + zipValue);
+                // This line of code will grab the input from the textbox...
+
+            });
+
+
+ 	 // document.getElementById("zipbutton").onclick = function () {
+   //      location.href = "survey.html";
+   //  };
+
+
+ });
+
+ 
