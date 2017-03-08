79851f0f1a0f39a91d4e4491e54a07008bb8ea0d
reza8793 <aabed525@gmail.com>
2017-03-07 23:05:30 -0800

    location in index.html and survey questions with jquery plugged

--- assets/javascript/frontEnd.js
+++ assets/javascript/frontEnd.js
@@ -3,13 +3,25 @@
 
  $(document).ready(function(){
 
- 	var zipValue;
+ 	var locationValue;
 
 
  	$('#findroomiebutton').on('click', function(){
 
                     console.log("find your roomie button clicked");
                 // This line of code will grab the input from the textbox...
+
+                var ans1 = document.getElementById("q1");
+                q1Value = ans1.options[ans1.selectedIndex].text;
+                console.log( "q1 value is " + q1Value);
+
+                var ans2 = document.getElementById("q2");
+                q2Value = ans2.options[ans2.selectedIndex].text;
+                console.log( "q2 value is " + q2Value);
+
+                var ans3 = document.getElementById("q3");
+                q3Value = ans3.options[ans3.selectedIndex].text;
+                console.log( "q3 value is " + q3Value);
 
             });
 
@@ -20,11 +32,17 @@
 
                    // location.href = "survey.html";
 
+                    var val = document.getElementById("locationdiv");
 
-                     zipValue = $('#zipinput').val().trim();
+
+                     locationValue = val.options[val.selectedIndex].text;
+ 
 
 
-                     console.log( "zip code is " + zipValue);
+                     console.log( "location is " + locationValue);
+
+                     location.href = "survey.html";
+
                 // This line of code will grab the input from the textbox...
 
             });
