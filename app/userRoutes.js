b07589c7861398902b5d5a417a1832bede44e9b1
nnien001 <nnien001@gmail.com>
2017-03-15 20:35:25 -0700

    age now being upserted in user login

--- app/userRoutes.js
+++ app/userRoutes.js
@@ -1,4 +1,6 @@
 console.log('using userRoutes.js');
+var moment = require('moment');
+
 
 module.exports = function(router) {
 
@@ -35,19 +37,23 @@ module.exports = function(router) {
 
   router.post("/db/userInsert", function(req, res) {
 
-    FB.api("me?fields=id,name,friends,picture{url},email", { access_token: fblocal.appAccessToken}, function (res) {
+    FB.api("me?fields=id,name,friends,picture{url},email,birthday", { access_token: fblocal.appAccessToken}, function (res) {
       	if(!res || res.error) {
         	console.log(!res ? 'error occurred' : res.error);
         	return;
       	}
-      
+      	
+      	var stringDOB = moment(res.birthday);
+      	var age = moment().diff(stringDOB, 'years');
+
       	User.findOneAndUpdate(
       		{	FBid: res.id 	}, 
       		{
         		FBName: res.name,
         		photolink: res.picture.data.url,
         		FBEmail: res.email,
-        		friendList: res.friends.data
+        		friendList: res.friends.data,
+        		age: age
         	}, 
         	{
         		upsert: true
