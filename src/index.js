5b4add2600dbc87d148429e1b50827f5038d5451
Deonte Lang <deonte.lang@gmail.com>
2017-03-18 13:43:55 -0700

    webpack addition and configuration utilizing babel suite and babel-css-loader. Script link to bundle.js added to index.html

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Router, browserHistory } from 'react-router';
-import makeRoutes from './routes';
+import makeRoutes from './routes.jsx';
 
 const appEntry = <Router history={browserHistory}>{makeRoutes()}</Router>;
 ReactDOM.render(
